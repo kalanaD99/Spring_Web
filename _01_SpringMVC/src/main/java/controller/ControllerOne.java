629094dcc14964128e49d6fb67357d0f47d0ca93
Kalana Dananjaya <kalanadananjaya855@gmail.com>
2023-10-14 20:30:30 +0530

    create hml file

--- _01_SpringMVC/src/main/java/controller/ControllerOne.java
+++ _01_SpringMVC/src/main/java/controller/ControllerOne.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/one")
+@RequestMapping("one")
 public class ControllerOne {
     public ControllerOne(){
         System.out.println("Controller one initiated");
--- _01_SpringMVC/src/main/java/controller/ControllerTwo.java
+++ _01_SpringMVC/src/main/java/controller/ControllerTwo.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/controllertwo")
+@RequestMapping("two")
 public class ControllerTwo {
     public ControllerTwo(){
         System.out.println("Controller Two");
